6f64d1c97b3c81f528dcfcb4218bf45c9503ad79
Andrew <andrew.hall0703@gmail.com>
2021-01-22 13:23:20 -0500

    Updates

---
+++ precache-manifest.e351375bf3cd41e75e817eba9d35bff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d9590da7eb73b86e787ee873a23ac7",
+    "url": "/Nextngnr/index.html"
+  },
+  {
+    "revision": "09d23e95ff8451b9c8cd",
+    "url": "/Nextngnr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d1cad0f861787b1bfb29",
+    "url": "/Nextngnr/static/js/2.1730e4c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nextngnr/static/js/2.1730e4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d23e95ff8451b9c8cd",
+    "url": "/Nextngnr/static/js/main.4a44a63f.chunk.js"
+  },
+  {
+    "revision": "d8a49a5d97d358592c8d",
+    "url": "/Nextngnr/static/js/runtime-main.137f3fc0.js"
+  }
+]);

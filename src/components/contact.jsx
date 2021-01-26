706fe6a39a1a14a3265f9753ad56650e85e4d571
Andrew <andrew.hall0703@gmail.com>
2021-01-25 22:53:09 -0500

    Ajustes de parrafos y derecchos de autor

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -122,7 +122,7 @@ export class Contact extends Component {
         <div id="footer">
           <div className="container text-center">
             <p>
-              &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
+              &copy; 2021 Andrew Hall NEXTNGNR Land Page. Original design by{" "}
               <a href="http://www.templatewire.com" rel="nofollow">
                 TemplateWire
               </a>
